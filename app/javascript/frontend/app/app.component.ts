5fb2b961fe242c2fc0c6427acc8520f721b52074
Volodymyr Leshchenko <v.v.grimm@gmail.com>
2018-05-16 22:40:23 +0300

    frontend setup

--- app/javascript/frontend/app/app.component.ts
+++ app/javascript/frontend/app/app.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'hello-angular',
+  selector: 'frontend',
   template: `<h1>Hello {{name}}</h1>`
 })
 export class AppComponent {
-  name = 'Angular!';
+  name = 'Frontend!';
 }
---
+++ app/javascript/packs/frontend.js
@@ -0,0 +1,7 @@
+// Run this Angular example by adding the following HTML markup to your view:
+//
+// <frontend>Loading...</frontend>
+//
+// <%= javascript_pack_tag 'frontend' %>
+
+require('../frontend')
